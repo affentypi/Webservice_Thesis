2dcec80b38a8b9d130222c11f62c1260be7ba9ee
Jacob Fehn <>
2023-10-25 23:22:51 +0200

    Finished quantitative in testing.py

--- testing.py
+++ testing.py
@@ -24,8 +24,7 @@ class TestQuantitativeResults(unittest.TestCase):
                     expected_cs_mods_per_changes.append(int(ecs))
                     if int(ecs) == 0:
                         expected_cs_changes -= 1
-                print(f"C: {expected_cs_changes} and Modifications per Changes = {expected_cs_mods_per_changes}")
-                # Amendments
+
                 expected_ms = result.split(";")[1].replace("M: ", "").split("+")
                 expected_ms_changes = len(expected_ms)
                 expected_ms_mods_per_changes = []
@@ -33,7 +32,16 @@ class TestQuantitativeResults(unittest.TestCase):
                     expected_ms_mods_per_changes.append(int(ems))
                     if int(ems) == 0:
                         expected_ms_changes -= 1
+
+                overall_expected_changes = 0
+                for modcs in expected_cs_mods_per_changes:
+                    overall_expected_changes += modcs
+                for modms in expected_ms_mods_per_changes:
+                    overall_expected_changes += modms
+
+                print(f"C: {expected_cs_changes} and Modifications per Changes = {expected_cs_mods_per_changes}")
                 print(f"M: {expected_ms_changes} and Modifications per Changes = {expected_ms_mods_per_changes}")
+                print(f"Overall there are {overall_expected_changes} expected!")
 
                 celex_old, doc_old = html_processing.pars_html(url_old)
                 celex_new, doc_new = html_processing.pars_html(url_new)
@@ -41,14 +49,34 @@ class TestQuantitativeResults(unittest.TestCase):
                 fast_result = nlp_processing.process_changes("testfast" + celex_old + celex_new, html_result,True)  # fast test
                 #accurate_result = nlp_processing.process_changes("testaccurate" + celex_old + celex_new, html_result,False)  # accurate test
 
-                print(len(fast_result[0]) == expected_ms_changes + expected_cs_changes)
-                self.assertEqual(len(fast_result[0]), expected_ms_changes + expected_cs_changes)
-            elif "repealed" in result:
-                celex_old, doc_old = html_processing.pars_html(url_old)
-                celex_new, doc_new = html_processing.pars_html(url_new)
-                html_result = html_processing.find_changes_and_make_diff_of_surrounding_text(doc_old, doc_new)
-                fast_result = nlp_processing.process_changes("testfast" + celex_old + celex_new, html_result,True)  # fast test
-                self.assertEqual(fast_result, "REPEALED")
+                if "repealed" in result:
+                    print(fast_result is None)
+                    self.assertEqual(fast_result, None)
+                elif expected_ms_changes + expected_cs_changes == 0:
+                    print(fast_result is None)
+                    self.assertEqual(fast_result, None)
+                else:
+                    found_ms_changes = 0
+                    found_cs_changes = 0
+                    for change_name in fast_result[0]:
+                        if change_name.startswith("A") or change_name.startswith("M"):
+                            found_ms_changes += 1
+                        if change_name.startswith("C"):
+                            found_cs_changes += 1
+                    overall_found_changes = 0
+                    for frs in fast_result[1]:
+                        overall_found_changes += len(frs)
+                    overall_found_changes -= len(fast_result[1])
+                    print(len(fast_result[1]) == expected_ms_changes + expected_cs_changes)
+                    self.assertEqual(len(fast_result[1]), expected_ms_changes + expected_cs_changes)  # test 1
+                    print(len(fast_result[0]) == expected_ms_changes + expected_cs_changes)
+                    self.assertEqual(len(fast_result[0]), expected_ms_changes + expected_cs_changes)  # test 1
+                    print(found_ms_changes == expected_ms_changes)
+                    self.assertEqual(found_ms_changes, expected_ms_changes)  # test 2
+                    print(found_cs_changes == expected_cs_changes)
+                    self.assertEqual(found_cs_changes, expected_cs_changes)  # test 2
+                    print(overall_found_changes == overall_expected_changes)
+                    self.assertEqual(overall_found_changes, overall_expected_changes) # test 3
             else:
                 print("RESULT not usable")
 
@@ -103,29 +131,23 @@ class TestQuantitativeResults(unittest.TestCase):
                 [celex, language, celex_link, first_date, first_url, middle_date, middle_url, last_date, last_url,
                  amount_of_consolidated_versions, directory, topic, name, comment, retrieval_date, result_first_last,
                  result_middle_last, result_first_middle])
-            if row < 0:
+            if row < 0: # 0, 101, 201
                 continue
-            elif row > 300:
+            elif row > 300: # 100, 200, 300
                 break
             else:
-                if first_url is not None and last_url is not None and result_first_last is not None:
+                if (first_url is not None and "NULL" not in first_url and "Not" not in first_url
+                        and last_url is not None and "NULL" not in last_url and "Not" not in last_url
+                        and result_first_last is not None and "OVERFLOW" not in result_first_last):
                     with self.subTest(celex + first_date + last_date + result_first_last):
                         self.test_old_new(first_url, last_url, result_first_last)
-                if middle_url is not None and last_url is not None and result_middle_last is not None:
+                if (middle_url is not None and "NULL" not in middle_url and "Not" not in middle_url
+                        and last_url is not None and "NULL" not in last_url and "Not" not in last_url
+                        and result_middle_last is not None and "OVERFLOW" not in result_middle_last):
                     with self.subTest(celex + middle_date + last_date + result_middle_last):
                         self.test_old_new(middle_url, last_url, result_middle_last)
-
-    def test_upper(self): #testing the test
-        self.assertEqual('foo'.upper(), 'FOO')
-
-
-# URL to online html file (32019R0817)
-url_first = "https://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=CELEX:32019R0817&from=DE"
-url_middle = "https://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=CELEX:02019R0817-20190522&from=DE" # C: 1 ; M: 0 ;
-url_latest = "https://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=CELEX%3A02019R0817-20210803&from=DE" # C: 1 ; M: 1 ;
-# Some Test File
-t_old = "https://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=CELEX:32003D0076"
-t_middle = "https://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=CELEX:02003D0076-20180510" # C: 0 ; M: 2 ;
-t_new = "https://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=CELEX:02003D0076-20210811" # C: 0 ; M: 2 + 6 ;
-
-#test_first_last(t_old, t_new, "C: 0 ; M: 2 + 6 ;")
+                if (first_url is not None and "NULL" not in first_url and "Not" not in first_url
+                    and middle_url is not None and "NULL" not in middle_url and "Not" not in middle_url
+                    and result_first_middle is not None and "OVERFLOW" not in result_first_middle):
+                    with self.subTest(celex + first_date + middle_date + result_first_middle):
+                        self.test_old_new(first_url, middle_url, result_first_middle)
